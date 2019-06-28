1546805393f824a8da0657a1c6c11abaa6483b3e
Suraj Pandey <psuraj61@gmail.com>
2019-06-28 13:24:14 +0530

    Full my sauce. No Yasir sauce

--- userbot/modules/extras.py
+++ userbot/modules/extras.py
@@ -64,7 +64,7 @@ async def _(event):
 @register(outgoing=True, pattern="^.sauce$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("https://github.com/Yasir-siddiqui/Userbot/")
+        await e.edit("https://github.com/Psuraj61/userbutt")
 
 @register(outgoing=True, pattern="^.readme$")
 async def reedme(e):
